c8b5f107d3a9e591dbcd9227693d662ad3337f24
Kippei Watanabe <k6play7210@gmail.com>
2020-05-07 11:05:05 +0900

    first commit

---
+++ puzmon0.cpp
@@ -0,0 +1,19 @@
+/*=== puzmon0: source code ===*/
+/*** include ***/
+
+#include <bits/stdc++.h>
+
+/*** enum ***/
+/*** global const ***/
+/*** struct ***/
+/*** prototype ***/
+/*** function ***/
+
+int main(int argc, char** argv)
+{
+    printf("*** Puzzele & Monsters ***\n");
+    return 0;
+}
+
+/*** utility ***/
+
